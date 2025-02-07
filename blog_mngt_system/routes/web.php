3dbb5d19808595331b122ed6d6fce761e571045e
Krishna Luharuka <rahdhekrishnaa@gmail.com>
2025-02-07 06:24:14 +0545

    created database configurations for users table and migrations

--- blog_mngt_system/routes/web.php
+++ blog_mngt_system/routes/web.php
@@ -2,12 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Auth\RegisteredUserController;
-// use App\Http\Controllers\Auth\ForgotPasswordController;
-use App\Http\Controllers\Auth\PasswordResetLinkController;
-use App\Http\Controllers\Auth\EmailVerificationNotificationController;
-use App\Http\Controllers\Auth\VerifyEmailController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -23,14 +18,5 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/login', [AuthenticatedSessionController::class, 'create'])
-    ->name('login');
-
-Route::post('/login', [AuthenticatedSessionController::class, 'store']);
-
-Route::get('/register', [RegisteredUserController::class, 'create'])
-    ->name('register');
-
-Route::post('/register', [RegisteredUserController::class, 'store']);
 
 require __DIR__.'/auth.php';
